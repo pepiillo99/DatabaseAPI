7c5f84842e34078d971c6f92054bcd2a07b74868
pepiillo99 <pepiillo99@hotmail.com>
2025-10-01 13:52:28 +0200

    Update CallbackRequest.java

--- src/me/pepe/DatabaseAPI/Utils/CallbackRequest.java
+++ src/me/pepe/DatabaseAPI/Utils/CallbackRequest.java
@@ -1,10 +1,23 @@
 package me.pepe.DatabaseAPI.Utils;
 
-public class CallbackRequest {
+public class CallbackRequest<T> {
+	private T result;
 	private String error;
 	public CallbackRequest() {}
+	public CallbackRequest(T result) {
+		this.result = result;
+	}
 	public CallbackRequest(String error) {
 		this.error = error;
+	}
+	public CallbackRequest(T result, String error) {
+		this.error = error;
+	}
+	public boolean hasResult() {
+		return result != null;
+	}
+	public T getResult() {
+		return result;
 	}
 	public boolean isCorrect() {
 		return !hasError();
